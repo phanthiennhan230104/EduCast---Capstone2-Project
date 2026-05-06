adce84d35096331c490c9ecb342e69d3cb6ea6e0
phanthiennhan230104 <phanthiennhan230104@gmail.com>
2026-05-07 00:18:48 +0700

    feat: update database configuration to use environment variables

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -104,11 +104,11 @@ AUTH_USER_MODEL = 'users.User'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'educast_db',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'NAME': os.getenv("DB_NAME"),
+        'USER': os.getenv("DB_USER"),
+        'PASSWORD': os.getenv("DB_PASSWORD"),
+        'HOST': os.getenv("DB_HOST"),
+        'PORT': os.getenv("DB_PORT"),
         'OPTIONS': {
             'charset': 'utf8mb4',
         }
